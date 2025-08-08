14e52886336f9c24265a74e19147a30ac34ce932
Renan Costa <105220100+RenanCosta2@users.noreply.github.com>
2025-08-08 17:39:53 -0300

    feat: sorteio de amigos

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []
+
+function adicionarAmigo(){
+    nome_amigo = document.getElementById("amigo").value
+    
+    if(nome_amigo == ""){
+        alert("Por favor, insira um nome.")
+    } else{
+        amigos.push(nome_amigo)
+        document.getElementById("amigo").value = ""
+    }
+}
+
+function atualizarLista(){
+    let lista = document.getElementById("listaAmigos")
+    lista.innerHTML = ""
+
+    for (let amigo of amigos){
+        let li = document.createElement("li")
+        li.textContent = amigo
+        lista.appendChild(li)
+    }
+}
+
+function sortearAmigo(){
+    qtd_amigos = amigos.length
+    if(qtd_amigos != 0){
+        numero_sorteado = Math.floor(Math.random() * qtd_amigos)
+        amigo_sorteado = amigos[numero_sorteado]
+
+        sorteado = document.getElementById("resultado")
+        sorteado.innerHTML = amigo_sorteado
+    } else{
+        alert("Por favor, adicione amigos antes de sortear.")
+    }
+}
